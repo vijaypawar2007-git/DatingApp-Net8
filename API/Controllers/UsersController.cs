e14403ccb61568a304aabc029387ba917d160706
Vijay Pawar <vijay.pawar@instem.com>
2024-12-04 14:16:52 +0530

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,12 +1,14 @@
+using System.Security.Claims;
 using API.DTOs;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -16,7 +18,7 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
         return Ok(users);
     }
 
-    [HttpGet("{username}")] // /api/users/1
+    [HttpGet("{username}", Name = "GetUser")] // /api/users/1
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
         var user = await userRepository.GetMemberAsync(username);
@@ -25,5 +27,23 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
             return NotFound();
 
         return user;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (username == null) return BadRequest("No username found in token");
+
+        var user = await userRepository.GetUserByUsernameAsync(username);
+
+        if (user == null) return BadRequest("Could not find user");
+
+        mapper.Map(memberUpdateDto, user);
+
+        if (await userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update the user");
     }
 }
