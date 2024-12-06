d74f6dc4f0cdb70d8c92ce04f24da3736873b911
Vijay Pawar <vijay.pawar@instem.com>
2024-12-06 14:51:52 +0530

    End of section 12

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -62,6 +62,8 @@ public class UsersController(IUserRepository userRepository, IPhotoService photo
             PublicId = result.PublicId
         };
 
+        if (user.Photos.Count == 0) photo.IsMain = true;
+
         user.Photos.Add(photo);
 
         if (await userRepository.SaveAllAsync())
